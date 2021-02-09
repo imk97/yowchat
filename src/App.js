d35c8acade863e35ec0e9a69c9a9258a93e8cb5e
imk97 <itmam97@gmail.com>
2021-02-09 16:50:02 +0800

    create name form

--- src/App.js
+++ src/App.js
@@ -1,80 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
-import { Form } from 'react-bootstrap';
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import CssBaseLine from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+
+const useStyles = (theme) => ({
+  root: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  button: {
+    marginTop: theme.spacing(1),
+    width: '100%',
+  }
+});
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.state({ value: event.target.value });
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
-
+  
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
+    const { classes } = this.props;
+    return(
+    <React.Fragment>
+      <CssBaseLine/>
+      <Container maxWidth="sm">
+        <div className={classes.root}>
           <img src={logo} className="App-logo" alt="logo" />
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Name : 
-              <input type="text" value={this.state.value } onChange={this.handleChange}/>
-            </label>
+          <form className={classes.form} noValidate autoComplete="false">
+            <TextField id="name" label="Name" fullWidth/>
+            <Button variant="contained" color="primary" className={classes.button} endIcon={<Icon>send</Icon>} >Send</Button>
           </form>
-        </header>
-      </div>
-    );
+        </div>
+      </Container>
+    </React.Fragment>
+    )
   }
 }
-// function App() {
-//   constructor(props) {
-//     super(props);
-//     this.state = {value: ''};
 
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-
-//   handleChange(event) {
-//     this.setState({value: event.target.value});
-//   }
-
-//   handleSubmit(event) {
-//     alert('A name was submitted: ' + this.state.value);
-//     event.preventDefault();
-//   }
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//     <form onSubmit={this.handleSubmit}>
-//       <label>Name : <input type="text" value={this.state.value} onChange={this.handleChange} /></label>
-//     </form>
-//   );
-// }
-
-export default App;
+export default withStyles(useStyles)(App);
